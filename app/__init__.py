082de36e9605b5ed12a8d70eca6a42418924bcd3
Omulosi <mulongojohnpaul@gmail.com>
2019-03-12 20:05:49 +0300

    set up and configure flask-jwt-extended

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
 migrate = Migrate()
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config=Config):
     """Create the application instance"""
@@ -17,6 +19,7 @@ def create_app(config=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     from app.api.v1 import bp
     app.register_blueprint(bp, url_prefix='/api/v1')
--- config.py
+++ config.py
@@ -5,8 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv('SECRET_KEY')
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL')

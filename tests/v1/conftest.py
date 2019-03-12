a67be26212518e38b82515a92c65703240e72854
Omulosi <mulongojohnpaul@gmail.com>
2019-03-12 22:35:54 +0300

    update auth fixture to generate access_token

--- tests/v1/conftest.py
+++ tests/v1/conftest.py
@@ -5,6 +5,7 @@ tests.v1.conftest
 setup functions to be used by test modules
 """
 
+import json
 import pytest
 from app import create_app, db
 from config import TestConfig
@@ -31,17 +32,36 @@ class AuthActions(object):
     """
     This class provides methods for authenticating users.
     """
+
+    DEFAULT_USER_LOGINS = {'email':'user@example.com', 'password':'r@78hy'}
+
     def __init__(self, client):
         self._client = client
+        self.access_token = None
 
     def signup(self, **kwargs):
-        return self._client.post('/api/v1/auth/signup', data=kwargs)
+        user_data = kwargs
+        if not kwargs:
+            user_data = self.DEFAULT_USER_LOGINS
+        response =  self._client.post('/api/v1/auth/signup', data=user_data)
+        data = json.loads(response.data.decode('utf-8'))
+        self.access_token = data['data'][0]['access_token']
+
+        return response
 
     def login(self, **kwargs):
-        return self._client.post('/api/v1/auth/signin', data=kwargs)
+        user_data = kwargs
+        if not kwargs:
+            user_data = self.DEFAULT_USER_LOGINS
+        response =  self._client.post('/api/v1/auth/signin', data=user_data)
+        data = json.loads(response.data.decode('utf-8'))
+        self.access_token = data['data'][0]['access_token']
+
+        return response
+
 
     def logout(self, **kwargs):
-        return self._client.post('/api/v1/auth/logout', data=kwargs)
+        return self._client.delete('/api/v1/auth/logout')
 
 @pytest.fixture
 def auth(client):
